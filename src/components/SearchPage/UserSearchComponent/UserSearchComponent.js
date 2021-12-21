1c9b800af4cb53bd495c1690a7a6ec2eb346c3e0
Andrii Bosyk <andriybosik2002@ukr.net>
2021-12-21 21:11:21 +0200

    Add click handling for searched user

--- src/components/SearchPage/UserSearchComponent/UserSearchComponent.js
+++ src/components/SearchPage/UserSearchComponent/UserSearchComponent.js
@@ -1,9 +1,19 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
+import { pages } from "../../../constants/pages";
+import { generateUrl } from "../../../handlers/LinkHandler";
+import { formatLink } from "../../../handlers/StringHandler";
 import "./UserSearchComponent.css";
 
 function UserSearchComponent({user, className}) {
+    const navigate = useNavigate();
+
+    const navigateToUserProfile = () => {
+        navigate(generateUrl(formatLink(pages.profile, {nickname: user.nickname})));
+    }
+
     return (
-        <div className={className + " UserSearchComponent s-vflex clickable"}>
+        <div className={className + " UserSearchComponent s-vflex clickable"} onClick={navigateToUserProfile}>
             <div className="s-hflex-center">
                 <div className="avatar stretch-background border50p z-depth-1" style={{backgroundImage: `url(${user.avatar})`}}></div>
             </div>
